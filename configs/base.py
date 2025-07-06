9afcd9c5219789feb9d8cd6e2ba1e6b67af713b3
Eugene <strahlt3@gmail.com>
2025-07-06 23:39:57 +0300

    Update base.py

--- configs/base.py
+++ configs/base.py
@@ -11,7 +11,5 @@ PROJECT_PATH = '/kaggle/working/geodata_dls'
 # для отладки из vs code
 # PROJECT_PATH = 'D:\it\geodata_dls' 
 
-# # на случай отдельного создания патчей
-# PROJECT_PATH_PATCH = 'D:\it\Geodata_project' 
 
 DEFAULT_SIZE = 512
