9eb30571b9a1d407ed0ca85b92bde02976166ed4
Eugene <strahlt3@gmail.com>
2025-07-07 17:04:34 +0300

    upd strmlt

--- app.py
+++ app.py
@@ -6,16 +6,18 @@ import os
 import torch
 
 
-model_weights_path = os.path.join(PROJECT_PATH, 'experiments/checkpoints/unet_resnet34_buildings_best.pth')
+APP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+model_weights_path = os.path.join(APP_PATH, 'experiments/checkpoints/unet_resnet34_buildings_best.pth')
 
 # Интерфейс
 st.title("Сегментация зданий")
 uploaded_file = st.file_uploader("Загрузите спутниковый снимок", type=["jpg", "png", "tif"])
 
 if uploaded_file:
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    DEVICE = torch.device('cpu')
     model = load_model(
         weights_path=model_weights_path,
-        device=device
+        device=DEVICE
     )
     show_results(model, uploaded_file)
